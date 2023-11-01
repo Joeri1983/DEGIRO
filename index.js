8006e7d6fd26dc9012b0acd1e234461aa91a6e98
Joeri1983 <96566809+Joeri1983@users.noreply.github.com>
2023-11-01 21:08:18 +0100

    Update index.js

--- index.js
+++ index.js
@@ -2,9 +2,43 @@ const http = require('http');
 const port = process.env.PORT || 3000;
 
 const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  const msg = 'Hello Node!\n'
-  res.end(msg);
+  if (req.method === 'GET') {
+    // Serve a basic HTML form
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/html');
+    res.write('<html><body>');
+    res.write('<form method="POST">');
+    res.write('<label for="numericValue">Enter a numeric value:</label>');
+    res.write('<input type="number" id="numericValue" name="numericValue"><br>');
+    res.write('<input type="submit" value="Submit">');
+    res.write('</form>');
+    res.write('</body></html>');
+    res.end();
+  } else if (req.method === 'POST') {
+    // Handle form submission
+    let data = '';
+    req.on('data', (chunk) => {
+      data += chunk;
+    });
+
+    req.on('end', () => {
+      // Parse the form data
+      const parsedData = new URLSearchParams(data.toString());
+      const numericValue = parseFloat(parsedData.get('numericValue'));
+
+      // Get the current date
+      const currentDate = new Date().toLocaleString();
+
+      // Display the entered value and the current date
+      res.statusCode = 200;
+      res.setHeader('Content-Type', 'text/html');
+      res.write('<html><body>');
+      res.write(`<p>Entered numeric value: ${numericValue}</p>`);
+      res.write(`<p>Current Date: ${currentDate}</p>`);
+      res.write('</body></html>');
+      res.end();
+    });
+  }
 });
 
 server.listen(port, () => {
