27789040a773c15ce174d8291d3f63f739407195
Joeri1983 <96566809+Joeri1983@users.noreply.github.com>
2023-11-01 21:14:23 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const http = require('http');
+const axios = require('axios'); // Import the axios library
 const port = process.env.PORT || 3000;
 
-const server = http.createServer((req, res) => {
+const server = http.createServer(async (req, res) => {
   if (req.method === 'GET') {
     // Serve a basic HTML form
     res.statusCode = 200;
@@ -12,32 +13,22 @@ const server = http.createServer((req, res) => {
     res.write('<input type="number" id="numericValue" name="numericValue"><br>');
     res.write('<input type="submit" value="Submit">');
     res.write('</form>');
+    res.write('<p>Contents of waardes.csv:</p>');
+    
+    // Fetch and display the contents of waardes.csv
+    try {
+      const response = await axios.get('https://storagejoeri.blob.core.windows.net/dgjoeri/waardes.csv?sp=r&st=2023-11-01T12:27:32Z&se=2023-11-01T20:27:32Z&spr=https&sv=2022-11-02&sr=c&sig=ndoa3k5uFv0yLUFOR17nkKFhj2zpJaKe3gzixg7z9yw%3D');
+      const csvContent = response.data;
+      res.write(`<pre>${csvContent}</pre>`);
+    } catch (error) {
+      res.write('Error fetching the CSV file.');
+    }
+    
     res.write('</body></html>');
     res.end();
   } else if (req.method === 'POST') {
     // Handle form submission
-    let data = '';
-    req.on('data', (chunk) => {
-      data += chunk;
-    });
-
-    req.on('end', () => {
-      // Parse the form data
-      const parsedData = new URLSearchParams(data.toString());
-      const numericValue = parseFloat(parsedData.get('numericValue'));
-
-      // Get the current date
-      const currentDate = new Date().toLocaleString();
-
-      // Display the entered value and the current date
-      res.statusCode = 200;
-      res.setHeader('Content-Type', 'text/html');
-      res.write('<html><body>');
-      res.write(`<p>Entered numeric value: ${numericValue}</p>`);
-      res.write(`<p>Current Date: ${currentDate}</p>`);
-      res.write('</body></html>');
-      res.end();
-    });
+    // (The form submission code remains the same as in the previous answer)
   }
 });
 
