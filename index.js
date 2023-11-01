555b7eaf02baf1297888810dc4f3eeca1d827530
Joeri1983 <96566809+Joeri1983@users.noreply.github.com>
2023-11-01 20:56:24 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,43 @@
-const http = require('http');
-const port = process.env.PORT || 3000;
+const azure = require('azure-storage');
+const blobService = azure.createBlobService();
 
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  const msg = 'Hello Node!\n'
-  res.end(msg);
-});
+// Use the extracted parts from the URL
+const accountName = 'storagejoeri';
+const sasToken = 'sp=r&st=2023-11-01T12:27:32Z&se=2023-11-01T20:27:32Z&spr=https&sv=2022-11-02&sr=c&sig=ndoa3k5uFv0yLUFOR17nkKFhj2zpJaKe3gzixg7z9yw%3D';
 
-server.listen(port, () => {
-  console.log(`Server running on http://localhost:${port}/`);
+// Create the connection string
+const connectionString = `DefaultEndpointsProtocol=https;AccountName=${accountName};SharedAccessSignature=${sasToken}`;
+
+blobService.withFilter(new azure.SASFilter());
+blobService._init({ storageAccount: '', storageAccessKey: '', host: accountName });
+
+const containerName = 'dgjoeri';
+const blobName = 'numericData.csv';
+const dataToSave = 'yourData';
+
+// Check if the blob exists, and if not, create it
+blobService.doesBlobExist(containerName, blobName, (error, result) => {
+  if (error) {
+    console.error('Error checking if blob exists', error);
+  } else {
+    if (!result.exists) {
+      // Blob doesn't exist, so create it
+      blobService.createBlockBlobFromText(containerName, blobName, dataToSave, (createError, createResult) => {
+        if (createError) {
+          console.error('Error creating blob', createError);
+        } else {
+          console.log('Blob created successfully');
+        }
+      });
+    } else {
+      // Blob already exists, update it if needed
+      blobService.createBlockBlobFromText(containerName, blobName, dataToSave, (updateError, updateResult) => {
+        if (updateError) {
+          console.error('Error updating blob', updateError);
+        } else {
+          console.log('Blob updated successfully');
+        }
+      });
+    }
+  }
 });
