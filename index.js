5be0890ce33e0bfec83f6301dcd3375e44bcc3c1
Joeri1983 <96566809+Joeri1983@users.noreply.github.com>
2023-11-01 21:01:47 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,43 +1,33 @@
-const azure = require('azure-storage');
-const blobService = azure.createBlobService();
+const express = require('express');
+const app = express();
+const port = 3000;
 
-// Use the extracted parts from the URL
-const accountName = 'storagejoeri';
-const sasToken = 'sp=r&st=2023-11-01T12:27:32Z&se=2023-11-01T20:27:32Z&spr=https&sv=2022-11-02&sr=c&sig=ndoa3k5uFv0yLUFOR17nkKFhj2zpJaKe3gzixg7z9yw%3D';
+app.use(express.urlencoded({ extended: true }));
 
-// Create the connection string
-const connectionString = `DefaultEndpointsProtocol=https;AccountName=${accountName};SharedAccessSignature=${sasToken}`;
+app.get('/', (req, res) => {
+  const html = `
+    <!DOCTYPE html>
+    <html>
+    <head>
+      <title>Value Display</title>
+    </head>
+    <body>
+      <form action="/display" method="post">
+        <label for="value">Enter a value:</label>
+        <input type="text" name="value" id="value" />
+        <button type="submit">Display</button>
+      </form>
+    </body>
+    </html>
+  `;
+  res.send(html);
+});
 
-blobService.withFilter(new azure.SASFilter());
-blobService._init({ storageAccount: '', storageAccessKey: '', host: accountName });
+app.post('/display', (req, res) => {
+  const value = req.body.value;
+  res.send(`You entered: ${value}`);
+});
 
-const containerName = 'dgjoeri';
-const blobName = 'numericData.csv';
-const dataToSave = 'yourData';
-
-// Check if the blob exists, and if not, create it
-blobService.doesBlobExist(containerName, blobName, (error, result) => {
-  if (error) {
-    console.error('Error checking if blob exists', error);
-  } else {
-    if (!result.exists) {
-      // Blob doesn't exist, so create it
-      blobService.createBlockBlobFromText(containerName, blobName, dataToSave, (createError, createResult) => {
-        if (createError) {
-          console.error('Error creating blob', createError);
-        } else {
-          console.log('Blob created successfully');
-        }
-      });
-    } else {
-      // Blob already exists, update it if needed
-      blobService.createBlockBlobFromText(containerName, blobName, dataToSave, (updateError, updateResult) => {
-        if (updateError) {
-          console.error('Error updating blob', updateError);
-        } else {
-          console.log('Blob updated successfully');
-        }
-      });
-    }
-  }
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
 });
