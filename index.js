1362f083e9d7ef60982519f33a1d21aa8ac3d08a
Joeri1983 <96566809+Joeri1983@users.noreply.github.com>
2023-11-01 21:17:28 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const http = require('http');
-const axios = require('axios'); // Import the axios library
+const https = require('https');
 const port = process.env.PORT || 3000;
 
 const server = http.createServer(async (req, res) => {
@@ -17,15 +17,26 @@ const server = http.createServer(async (req, res) => {
     
     // Fetch and display the contents of waardes.csv
     try {
-      const response = await axios.get('https://storagejoeri.blob.core.windows.net/dgjoeri/waardes.csv?sp=r&st=2023-11-01T12:27:32Z&se=2023-11-01T20:27:32Z&spr=https&sv=2022-11-02&sr=c&sig=ndoa3k5uFv0yLUFOR17nkKFhj2zpJaKe3gzixg7z9yw%3D');
-      const csvContent = response.data;
-      res.write(`<pre>${csvContent}</pre>`);
+      const azureStorageUrl = 'https://storagejoeri.blob.core.windows.net/dgjoeri/waardes.csv?sp=r&st=2023-11-01T12:27:32Z&se=2023-11-01T20:27:32Z&spr=https&sv=2022-11-02&sr=c&sig=ndoa3k5uFv0yLUFOR17nkKFhj2zpJaKe3gzixg7z9yw%3D';
+
+      https.get(azureStorageUrl, (azureRes) => {
+        let csvContent = '';
+
+        azureRes.on('data', (chunk) => {
+          csvContent += chunk;
+        });
+
+        azureRes.on('end', () => {
+          res.write(`<pre>${csvContent}</pre>`);
+          res.write('</body></html>');
+          res.end();
+        });
+      });
     } catch (error) {
       res.write('Error fetching the CSV file.');
+      res.write('</body></html>');
+      res.end();
     }
-    
-    res.write('</body></html>');
-    res.end();
   } else if (req.method === 'POST') {
     // Handle form submission
     // (The form submission code remains the same as in the previous answer)
