4e9b2ac9e396a5e178570079d2c7809fe49d3b9a
Joeri1983 <96566809+Joeri1983@users.noreply.github.com>
2023-11-01 21:06:07 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,33 +1,12 @@
-const express = require('express');
-const app = express();
-const port = 3000;
+const http = require('http');
+const port = process.env.PORT || 3000;
 
-app.use(express.urlencoded({ extended: true }));
-
-app.get('/', (req, res) => {
-  const html = `
-    <!DOCTYPE html>
-    <html>
-    <head>
-      <title>Value Display</title>
-    </head>
-    <body>
-      <form action="/display" method="post">
-        <label for="value">Enter a value:</label>
-        <input type="text" name="value" id="value" />
-        <button type="submit">Display</button>
-      </form>
-    </body>
-    </html>
-  `;
-  res.send(html);
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  const msg = 'Hello Node!\n'
+  res.end(msg);
 });
 
-app.post('/display', (req, res) => {
-  const value = req.body.value;
-  res.send(`You entered: ${value}`);
-});
-
-app.listen(port, () => {
-  console.log(`Server is running on http://localhost:${port}`);
+server.listen(port, () => {
+  console.log(`Server running on http://localhost:${port}/`);
 });
