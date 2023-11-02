f0a7f11d87122eec3350fd315549e6df08bbb2c7
Joeri1983 <96566809+Joeri1983@users.noreply.github.com>
2023-11-02 13:47:02 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,24 @@
 const http = require('http');
 const https = require('https');
 const fs = require('fs');
+const querystring = require('querystring');
 const port = process.env.PORT || 3000;
 
 const azureStorageUrl = 'https://storagejoeri.blob.core.windows.net/dgjoeri/waardes.csv';
 
 const server = http.createServer((req, res) => {
   if (req.method === 'GET') {
+    // Serve a basic HTML page with an input form
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/html');
+    res.write('<html><body>');
+    res.write('<p>Enter a numeric value:</p>');
+    res.write('<form method="POST">');
+    res.write('<input type="number" name="numericValue"><br>');
+    res.write('<input type="submit" value="Submit">');
+    res.write('</form>');
+    res.write('<p>Values from waardes.csv:</p>');
+
     // Fetch and display the contents of waardes.csv
     https.get(azureStorageUrl, (response) => {
       let data = '';
@@ -16,53 +28,56 @@ const server = http.createServer((req, res) => {
 
       response.on('end', () => {
         const lines = data.trim().split('\n');
-        const values = lines.map((line) => {
+        res.write('<ul>');
+        lines.forEach((line) => {
           const columns = line.split(';');
-          return {
-            date: columns[0],
-            value: columns[1],
-          };
+          res.write(`<li>Date: ${columns[0]}, Value: ${columns[1]}</li>`);
         });
-
-        res.statusCode = 200;
-        res.setHeader('Content-Type', 'text/html');
-        res.write('<html><body>');
-        res.write('<p>Values from waardes.csv:</p>');
-
-        // Create a canvas for the chart
-        res.write('<canvas id="myChart" width="400" height="200"></canvas>');
-
-        // Generate the chart using Chart.js
-        res.write('<script src="https://cdn.jsdelivr.net/npm/chart.js"></script>');
-        res.write('<script>');
-        res.write('var ctx = document.getElementById("myChart").getContext("2d");');
-        res.write('var labels = ' + JSON.stringify(values.map((line) => line.date)) + ';');
-        res.write('var data = ' + JSON.stringify(values.map((line) => line.value)) + ';');
-        res.write('var myChart = new Chart(ctx, {');
-        res.write('type: "line",');
-        res.write('data: {');
-        res.write('labels: labels,');
-        res.write('datasets: [{');
-        res.write('label: "Values",');
-        res.write('data: data,');
-        res.write('backgroundColor: "rgba(75, 192, 192, 0.2)",');
-        res.write('borderColor: "rgba(75, 192, 192, 1)",');
-        res.write('borderWidth: 1');
-        res.write('}]');
-        res.write('},');
-        res.write('options: {');
-        res.write('scales: {');
-        res.write('y: {');
-        res.write('beginAtZero: true');
-        res.write('}');
-        res.write('}');
-        res.write('}');
-        res.write('});');
-        res.write('</script>');
-
+        res.write('</ul>');
         res.write('</body></html>');
         res.end();
       });
+    });
+  } else if (req.method === 'POST') {
+    let body = '';
+    req.on('data', (chunk) => {
+      body += chunk;
+    });
+
+    req.on('end', () => {
+      const postData = querystring.parse(body);
+      const numericValue = postData.numericValue;
+
+      if (isNaN(numericValue)) {
+        res.statusCode = 400;
+        res.setHeader('Content-Type', 'text/plain');
+        res.end('Invalid input. Please enter a numeric value.');
+        return;
+      }
+
+      // Append the new value to waardes.csv
+      const newValue = new Date().toISOString().slice(0, 10).replace(/-/g, '') + ';' + numericValue + '\n';
+      const options = {
+        method: 'PUT',
+        headers: {
+          'Content-Type': 'text/plain',
+          'Content-Length': Buffer.byteLength(newValue).toString(),
+        },
+      };
+
+      const req = https.request(azureStorageUrl, options, (response) => {
+        if (response.statusCode === 201) {
+          res.statusCode = 302;
+          res.setHeader('Location', '/');
+          res.end();
+        } else {
+          res.statusCode = response.statusCode;
+          res.end('Error appending to the CSV file in Azure Blob Storage.');
+        }
+      });
+
+      req.write(newValue);
+      req.end();
     });
   }
 });
