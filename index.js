e00f676bf6bc5cc66bc673dfa6310ff2375b82f4
Joeri1983 <96566809+Joeri1983@users.noreply.github.com>
2023-11-02 11:49:34 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,17 +1,12 @@
 const http = require('http');
 const https = require('https');
+const fs = require('fs');
 const port = process.env.PORT || 3000;
 
 const azureStorageUrl = 'https://storagejoeri.blob.core.windows.net/dgjoeri/waardes.csv';
 
 const server = http.createServer((req, res) => {
   if (req.method === 'GET') {
-    // Serve a basic HTML page
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/html');
-    res.write('<html><body>');
-    res.write('<p>Latest 3 values from waardes.csv sorted by date:</p>');
-
     // Fetch and display the contents of waardes.csv
     https.get(azureStorageUrl, (response) => {
       let data = '';
@@ -23,20 +18,57 @@ const server = http.createServer((req, res) => {
         const lines = data.trim().split('\n');
         const sortedLines = lines
           .map((line) => {
-            const columns = line.split(';'); // Change the separator to semicolon
+            const columns = line.split(';');
             return {
               date: columns[0],
               value: columns[1],
             };
           })
-          .sort((a, b) => b.date.localeCompare(a.date)) // Sort in descending order
-          .slice(0, 3); // Take the latest 3 values
+          .sort((a, b) => b.date.localeCompare(a.date))
+          .slice(0, 3);
+
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'text/html');
+        res.write('<html><body>');
+        res.write('<p>Latest 3 values from waardes.csv sorted by date:</p>');
 
         res.write('<ul>');
         sortedLines.forEach((line) => {
           res.write(`<li>Date: ${line.date}, Value: ${line.value}</li>`);
         });
         res.write('</ul>');
+
+        // Create a canvas for the chart
+        res.write('<canvas id="myChart" width="400" height="200"></canvas>');
+
+        // Generate the chart using Chart.js
+        res.write('<script src="https://cdn.jsdelivr.net/npm/chart.js"></script>');
+        res.write('<script>');
+        res.write('var ctx = document.getElementById("myChart").getContext("2d");');
+        res.write('var labels = ' + JSON.stringify(sortedLines.map((line) => line.date)) + ';');
+        res.write('var values = ' + JSON.stringify(sortedLines.map((line) => line.value)) + ';');
+        res.write('var myChart = new Chart(ctx, {');
+        res.write('type: "line",');
+        res.write('data: {');
+        res.write('labels: labels,');
+        res.write('datasets: [{');
+        res.write('label: "Values",');
+        res.write('data: values,');
+        res.write('backgroundColor: "rgba(75, 192, 192, 0.2)",');
+        res.write('borderColor: "rgba(75, 192, 192, 1)",');
+        res.write('borderWidth: 1');
+        res.write('}]');
+        res.write('},');
+        res.write('options: {');
+        res.write('scales: {');
+        res.write('y: {');
+        res.write('beginAtZero: true');
+        res.write('}');
+        res.write('}');
+        res.write('}');
+        res.write('});');
+        res.write('</script>');
+
         res.write('</body></html>');
         res.end();
       });
