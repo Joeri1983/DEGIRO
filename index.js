100047c38fd72972c61d74da012cac953dfdcd9f
Joeri1983 <96566809+Joeri1983@users.noreply.github.com>
2023-11-02 11:53:04 +0100

    Update index.js

--- index.js
+++ index.js
@@ -16,27 +16,18 @@ const server = http.createServer((req, res) => {
 
       response.on('end', () => {
         const lines = data.trim().split('\n');
-        const sortedLines = lines
-          .map((line) => {
-            const columns = line.split(';');
-            return {
-              date: columns[0],
-              value: columns[1],
-            };
-          })
-          .sort((a, b) => b.date.localeCompare(a.date))
-          .slice(0, 3);
+        const values = lines.map((line) => {
+          const columns = line.split(';');
+          return {
+            date: columns[0],
+            value: columns[1],
+          };
+        });
 
         res.statusCode = 200;
         res.setHeader('Content-Type', 'text/html');
         res.write('<html><body>');
-        res.write('<p>Latest 3 values from waardes.csv sorted by date:</p>');
-
-        res.write('<ul>');
-        sortedLines.forEach((line) => {
-          res.write(`<li>Date: ${line.date}, Value: ${line.value}</li>`);
-        });
-        res.write('</ul>');
+        res.write('<p>Values from waardes.csv:</p>');
 
         // Create a canvas for the chart
         res.write('<canvas id="myChart" width="400" height="200"></canvas>');
@@ -45,15 +36,15 @@ const server = http.createServer((req, res) => {
         res.write('<script src="https://cdn.jsdelivr.net/npm/chart.js"></script>');
         res.write('<script>');
         res.write('var ctx = document.getElementById("myChart").getContext("2d");');
-        res.write('var labels = ' + JSON.stringify(sortedLines.map((line) => line.date)) + ';');
-        res.write('var values = ' + JSON.stringify(sortedLines.map((line) => line.value)) + ';');
+        res.write('var labels = ' + JSON.stringify(values.map((line) => line.date)) + ';');
+        res.write('var data = ' + JSON.stringify(values.map((line) => line.value)) + ';');
         res.write('var myChart = new Chart(ctx, {');
         res.write('type: "line",');
         res.write('data: {');
         res.write('labels: labels,');
         res.write('datasets: [{');
         res.write('label: "Values",');
-        res.write('data: values,');
+        res.write('data: data,');
         res.write('backgroundColor: "rgba(75, 192, 192, 0.2)",');
         res.write('borderColor: "rgba(75, 192, 192, 1)",');
         res.write('borderWidth: 1');
