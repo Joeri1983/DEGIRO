5ae255a8f325b0f4c8152bf1437cce61b98fcbe1
Joeri1983 <96566809+Joeri1983@users.noreply.github.com>
2023-11-03 11:22:10 +0100

    Update index.js

--- index.js
+++ index.js
@@ -20,13 +20,23 @@ const server = http.createServer((req, res) => {
           const [date, value] = line.split(':');
           return {
             date: date,
-            value: value,
+            value: parseFloat(value), // Convert value to a floating-point number
           };
         });
 
         res.statusCode = 200;
         res.setHeader('Content-Type', 'text/html');
         res.write('<html><body>');
+
+        // Add an input for selecting values
+        res.write('<select id="valueSelector">');
+        values.forEach((line, index) => {
+          res.write(`<option value="${index}">${line.date}</option>`);
+        });
+        res.write('</select>');
+
+        // Add a button to calculate and update the chart
+        res.write('<button onclick="calculatePercentageDifference()">Calculate</button>');
 
         // Create a canvas for the chart
         res.write('<canvas id="myChart" width="400" height="200"></canvas>');
@@ -35,15 +45,14 @@ const server = http.createServer((req, res) => {
         res.write('<script src="https://cdn.jsdelivr.net/npm/chart.js"></script>');
         res.write('<script>');
         res.write('var ctx = document.getElementById("myChart").getContext("2d");');
-        res.write('var labels = ' + JSON.stringify(values.map((line) => line.date)) + ';');
-        res.write('var data = ' + JSON.stringify(values.map((line) => line.value)) + ';');
-        res.write('var myChart = new Chart(ctx, {');
+        res.write('var chartData = ' + JSON.stringify(values.map((line) => line.value)) + ';');
+        res.write('var chart = new Chart(ctx, {');
         res.write('type: "line",');
         res.write('data: {');
-        res.write('labels: labels,');
+        res.write('labels: ' + JSON.stringify(values.map((line) => line.date)) + ',');
         res.write('datasets: [{');
         res.write('label: "Values",');
-        res.write('data: data,');
+        res.write('data: chartData,');
         res.write('backgroundColor: "rgba(75, 192, 192, 0.2)",');
         res.write('borderColor: "rgba(75, 192, 192, 1)",');
         res.write('borderWidth: 1');
@@ -57,7 +66,19 @@ const server = http.createServer((req, res) => {
         res.write('}');
         res.write('}');
         res.write('});');
+
+        // JavaScript function to calculate and update percentage difference
+        res.write('function calculatePercentageDifference() {');
+        res.write('var select = document.getElementById("valueSelector");');
+        res.write('var selectedIndex = select.value;');
+        res.write('var selectedValue = chartData[selectedIndex];');
+        res.write('var percentageDifference = ((selectedValue - chartData[0]) / chartData[0]) * 100;');
+        res.write('document.getElementById("percentageDifference").innerText = "Percentage Difference: " + percentageDifference.toFixed(2) + "%";');
+        res.write('}');
         res.write('</script>');
+
+        // Display the percentage difference
+        res.write('<div id="percentageDifference"></div>');
 
         res.write('</body></html>');
         res.end();
