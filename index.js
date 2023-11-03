be494ddfdcc17a3d7878e4e9f41f6686ec54080f
Joeri1983 <96566809+Joeri1983@users.noreply.github.com>
2023-11-03 08:45:04 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,52 +1,69 @@
 const http = require('http');
+const https = require('https');
 const fs = require('fs');
 const port = process.env.PORT || 3000;
 
-const azureSasUrl = 'https://storagejoeri.blob.core.windows.net/dgjoeri/waardes.csv?comp=appendblock&sp=racwdyti&st=2023-11-02T15:21:13Z&se=2024-07-31T22:21:13Z&spr=https&sv=2022-11-02&sr=b&sig=oChszCUdn7GFjNWwECHIzHxuNdoJ9QJyDuGyYf1VNMk%3D';
+const azureStorageUrl = 'https://storagejoeri.blob.core.windows.net/dgjoeri/waardes.csv';
 
 const server = http.createServer((req, res) => {
   if (req.method === 'GET') {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/html');
-    res.write('<html><body>');
+    // Fetch and display the contents of waardes.csv
+    https.get(azureStorageUrl, (response) => {
+      let data = '';
+      response.on('data', (chunk) => {
+        data += chunk;
+      });
 
-    // Input bar for user to enter a value
-    res.write('<div><label for="valueInput">Enter a Value:</label>');
-    res.write('<input type="text" id="valueInput" name="value" />');
-    res.write('<button onclick="submitValue()">Submit</button></div>');
+      response.on('end', () => {
+        const lines = data.trim().split('\n');
+        const values = lines.map((line) => {
+          const columns = line.split(';');
+          return {
+            date: columns[0],
+            value: columns[1],
+          };
+        });
 
-    res.write('<p>Values:</p>');
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'text/html');
+        res.write('<html><body>');
+        res.write('<p>Values:</p>');
 
-    // Create a canvas for the chart
-    res.write('<canvas id="myChart" width="400" height="200"></canvas>');
+        // Create a canvas for the chart
+        res.write('<canvas id="myChart" width="400" height="200"></canvas>');
 
-    // Generate the chart using Chart.js
-    res.write('<script src="https://cdn.jsdelivr.net/npm/chart.js"></script>');
-    res.write('<script>');
-    res.write('var ctx = document.getElementById("myChart").getContext("2d");');
-    // (You can keep your chart code here)
+        // Generate the chart using Chart.js
+        res.write('<script src="https://cdn.jsdelivr.net/npm/chart.js"></script>');
+        res.write('<script>');
+        res.write('var ctx = document.getElementById("myChart").getContext("2d");');
+        res.write('var labels = ' + JSON.stringify(values.map((line) => line.date)) + ';');
+        res.write('var data = ' + JSON.stringify(values.map((line) => line.value)) + ';');
+        res.write('var myChart = new Chart(ctx, {');
+        res.write('type: "line",');
+        res.write('data: {');
+        res.write('labels: labels,');
+        res.write('datasets: [{');
+        res.write('label: "Values",');
+        res.write('data: data,');
+        res.write('backgroundColor: "rgba(75, 192, 192, 0.2)",');
+        res.write('borderColor: "rgba(75, 192, 192, 1)",');
+        res.write('borderWidth: 1');
+        res.write('}]');
+        res.write('},');
+        res.write('options: {');
+        res.write('scales: {');
+        res.write('y: {');
+        res.write('beginAtZero: true');
+        res.write('}');
+        res.write('}');
+        res.write('}');
+        res.write('});');
+        res.write('</script>');
 
-    res.write('function submitValue() {');
-    res.write('var valueInput = document.getElementById("valueInput");');
-    res.write('var value = valueInput.value;');
-    res.write('var xhr = new XMLHttpRequest();');
-    res.write('xhr.open("POST", "' + azureSasUrl + '", true);');
-    res.write('xhr.setRequestHeader("Content-Type", "text/plain");');
-    res.write('xhr.onreadystatechange = function() {');
-    res.write('if (xhr.readyState === 4) {');
-    res.write('if (xhr.status === 202) {');
-    res.write('console.log("Value appended to the CSV file: " + value);');
-    res.write('} else {');
-    res.write('console.error("Error appending value to CSV file: " + xhr.statusText);');
-    res.write('}');
-    res.write('}');
-    res.write('};');
-    res.write('xhr.send(value);');
-    res.write('}');
-    res.write('</script>');
-
-    res.write('</body></html>');
-    res.end();
+        res.write('</body></html>');
+        res.end();
+      });
+    });
   }
 });
 
