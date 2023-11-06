5aee7e184870e63fb5de5341db8373f33c4eb1ed
Joeri1983 <96566809+Joeri1983@users.noreply.github.com>
2023-11-06 10:20:37 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,31 +1,71 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-
-const app = express();
+const http = require('http');
+const https = require('https');
+const fs = require('fs');
 const port = process.env.PORT || 3000;
 
-// Middleware to parse JSON payloads
-app.use(bodyParser.json());
+const azureStorageUrl = 'https://storagejoeri.blob.core.windows.net/dgjoeri/waardes.csv';
 
-// Define an API route that accepts JSON payload at /input
-app.post('/input', (req, res) => {
-  const payload = req.body;
+const server = http.createServer((req, res) => {
+  if (req.method === 'GET') {
+    // Fetch and display the contents of waardes.csv
+    https.get(azureStorageUrl, (response) => {
+      let data = '';
+      response.on('data', (chunk) => {
+        data += chunk;
+      });
 
-  // Check if "waarde" exists in the JSON payload
-  if (payload && payload.waarde) {
-    const waardeValue = payload.waarde;
-    res.status(200).json({ message: `Received "waarde" with value: ${waardeValue}` });
-  } else {
-    res.status(400).json({ error: 'Invalid JSON payload. "waarde" is missing.' });
+      response.on('end', () => {
+        const lines = data.trim().split(',');
+        const values = lines.map((line) => {
+          const [date, value] = line.split(':');
+          return {
+            date: date,
+            value: value,
+          };
+        });
+
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'text/html');
+        res.write('<html><body>');
+
+        // Create a canvas for the chart
+        res.write('<canvas id="myChart" width="400" height="200"></canvas>');
+
+        // Generate the chart using Chart.js
+        res.write('<script src="https://cdn.jsdelivr.net/npm/chart.js"></script>');
+        res.write('<script>');
+        res.write('var ctx = document.getElementById("myChart").getContext("2d");');
+        res.write('var labels = ' + JSON.stringify(values.map((line) => line.date)) + ';');
+        res.write('var data = ' + JSON.stringify(values.map((line) => line.value)) + ';');
+        res.write('var myChart = new Chart(ctx, {');
+        res.write('type: "line",');
+        res.write('data: {');
+        res.write('labels: labels,');
+        res.write('datasets: [{');
+        res.write('label: "Values",');
+        res.write('data: data,');
+        res.write('backgroundColor: "rgba(75, 192, 192, 0.2)",');
+        res.write('borderColor: "rgba(75, 192, 192, 1)",');
+        res.write('borderWidth: 1');
+        res.write('}]');
+        res.write('},');
+        res.write('options: {');
+        res.write('scales: {');
+        res.write('y: {');
+        res.write('beginAtZero: true');
+        res.write('}');
+        res.write('}');
+        res.write('}');
+        res.write('});');
+        res.write('</script>');
+
+        res.write('</body></html>');
+        res.end();
+      });
+    });
   }
 });
 
-// Define a route for a GET request to generate a random number
-app.get('/random', (req, res) => {
-  const randomValue = Math.floor(Math.random() * 100) + 1;
-  res.status(200).json({ value: randomValue });
-});
-
-app.listen(port, () => {
-  console.log(`Node.js API is running on port ${port}`);
+server.listen(port, () => {
+  console.log(`Server running on http://localhost:${port}/`);
 });
