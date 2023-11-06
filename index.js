7f1608e89c2884a2ef4351bd82f1a55c16fdf349
Joeri1983 <96566809+Joeri1983@users.noreply.github.com>
2023-11-06 10:13:26 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,18 +1,14 @@
-const http = require('http');
-const https = require('https');
 const express = require('express');
 const bodyParser = require('body-parser');
 
 const app = express();
-const port = process.env.PORT || 8080; // Change the port to 8080
-
-const azureStorageUrl = 'https://storagejoeri.blob.core.windows.net/dgjoeri/waardes.csv';
+const port = process.env.PORT || 3000;
 
 // Middleware to parse JSON payloads
 app.use(bodyParser.json());
 
-// Create an API route that accepts JSON payloads at /waarde
-app.post('/waarde', (req, res) => {
+// Define an API route that accepts JSON payload
+app.post('/your-api-endpoint', (req, res) => {
   const payload = req.body;
 
   // Check if "waarde" exists in the JSON payload
@@ -24,69 +20,6 @@ app.post('/waarde', (req, res) => {
   }
 });
 
-// Create your server with the existing code
-const server = http.createServer((req, res) => {
-  if (req.method === 'GET') {
-    // Fetch and display the contents of waardes.csv
-    https.get(azureStorageUrl, (response) => {
-      let data = '';
-      response.on('data', (chunk) => {
-        data += chunk;
-      });
-
-      response.on('end', () => {
-        const lines = data.trim().split(',');
-        const values = lines.map((line) => {
-          const [date, value] = line.split(':');
-          return {
-            date: date,
-            value: value,
-          };
-        });
-
-        res.statusCode = 200;
-        res.setHeader('Content-Type', 'text/html');
-        res.write('<html><body>');
-
-        // Create a canvas for the chart
-        res.write('<canvas id="myChart" width="400" height="200"></canvas>');
-
-        // Generate the chart using Chart.js
-        res.write('<script src="https://cdn.jsdelivr.net/npm/chart.js"></script>');
-        res.write('<script>');
-        res.write('var ctx = document.getElementById("myChart").getContext("2d");');
-        res.write('var labels = ' + JSON.stringify(values.map((line) => line.date)) + ';');
-        res.write('var data = ' + JSON.stringify(values.map((line) => line.value)) + ';');
-        res.write('var myChart = new Chart(ctx, {');
-        res.write('type: "line",');
-        res.write('data: {');
-        res.write('labels: labels,');
-        res.write('datasets: [{');
-        res.write('label: "Values",');
-        res.write('data: data,');
-        res.write('backgroundColor: "rgba(75, 192, 192, 0.2)",');
-        res.write('borderColor: "rgba(75, 192, 192, 1)",');
-        res.write('borderWidth: 1');
-        res.write('}]');
-        res.write('},');
-        res.write('options: {');
-        res.write('scales: {');
-        res.write('y: {');
-        res.write('beginAtZero: true');
-        res.write('}');
-        res.write('}');
-        res.write('}');
-        res.write('});');
-        res.write('</script>');
-
-        res.write('</body></html>');
-        res.end();
-      });
-    });
-  }
-});
-
-// Add the Express app to your server
 app.listen(port, () => {
-  console.log(`Server running on http://localhost:${port}/`);
+  console.log(`Node.js API is running on port ${port}`);
 });
