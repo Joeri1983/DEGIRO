e6fdf691c00b3d9ef5a4c014e4baee6f11572d8b
Joeri1983 <96566809+Joeri1983@users.noreply.github.com>
2023-11-10 13:52:06 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 const http = require('http');
 const https = require('https');
+const fs = require('fs');
 const port = process.env.PORT || 3000;
 
 const azureStorageUrl = 'https://storagejoeri.blob.core.windows.net/dgjoeri/waardes.csv';
 
 const server = http.createServer((req, res) => {
   if (req.method === 'GET') {
+    // Fetch and display the contents of waardes.csv
     https.get(azureStorageUrl, (response) => {
       let data = '';
       response.on('data', (chunk) => {
@@ -13,8 +15,8 @@ const server = http.createServer((req, res) => {
       });
 
       response.on('end', () => {
-        let lines = data.trim().split(',');
-        let values = lines.map((line) => {
+        const lines = data.trim().split(',');
+        const values = lines.map((line) => {
           const [date, value] = line.split(':');
           return {
             date: date,
@@ -22,62 +24,47 @@ const server = http.createServer((req, res) => {
           };
         });
 
-        let currentIndex = 0;
+        // Reverse the arrays to display values from newest to oldest
+        const reversedDates = values.map((line) => line.date).reverse();
+        const reversedValues = values.map((line) => line.value).reverse();
 
-        function sendValues() {
-          const slicedValues = values.slice(currentIndex, currentIndex + 25);
-          currentIndex += 25;
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'text/html');
+        res.write('<html><body>');
 
-          res.statusCode = 200;
-          res.setHeader('Content-Type', 'text/html');
-          res.write('<html><body>');
+        // Create a canvas for the chart
+        res.write('<canvas id="myChart" width="400" height="200"></canvas>');
 
-          res.write('<canvas id="myChart" width="400" height="200"></canvas>');
+        // Generate the chart using Chart.js
+        res.write('<script src="https://cdn.jsdelivr.net/npm/chart.js"></script>');
+        res.write('<script>');
+        res.write('var ctx = document.getElementById("myChart").getContext("2d");');
+        res.write('var labels = ' + JSON.stringify(reversedDates) + ';');
+        res.write('var data = ' + JSON.stringify(reversedValues) + ';');
+        res.write('var myChart = new Chart(ctx, {');
+        res.write('type: "line",');
+        res.write('data: {');
+        res.write('labels: labels,');
+        res.write('datasets: [{');
+        res.write('label: "Values",');
+        res.write('data: data,');
+        res.write('backgroundColor: "rgba(75, 192, 192, 0.2)",');
+        res.write('borderColor: "rgba(75, 192, 192, 1)",');
+        res.write('borderWidth: 1');
+        res.write('}]');
+        res.write('},');
+        res.write('options: {');
+        res.write('scales: {');
+        res.write('y: {');
+        res.write('beginAtZero: true');
+        res.write('}');
+        res.write('}');
+        res.write('}');
+        res.write('});');
+        res.write('</script>');
 
-          res.write('<button id="loadMore">Load 25 More Records</button>');
-
-          res.write('<script src="https://cdn.jsdelivr.net/npm/chart.js"></script>');
-          res.write('<script>');
-          res.write('var ctx = document.getElementById("myChart").getContext("2d");');
-          res.write('var labels = ' + JSON.stringify(slicedValues.map((line) => line.date)) + ';');
-          res.write('var data = ' + JSON.stringify(slicedValues.map((line) => line.value)) + ';');
-          res.write('var myChart = new Chart(ctx, {');
-          res.write('type: "line",');
-          res.write('data: {');
-          res.write('labels: labels,');
-          res.write('datasets: [{');
-          res.write('label: "Values",');
-          res.write('data: data,');
-          res.write('backgroundColor: "rgba(75, 192, 192, 0.2)",');
-          res.write('borderColor: "rgba(75, 192, 192, 1)",');
-          res.write('borderWidth: 1');
-          res.write('}]');
-          res.write('},');
-          res.write('options: {');
-          res.write('scales: {');
-          res.write('y: {');
-          res.write('beginAtZero: true');
-          res.write('}');
-          res.write('}');
-          res.write('}');
-          res.write('});');
-
-          res.write('document.getElementById("loadMore").addEventListener("click", function() {');
-          res.write('var moreData = ' + JSON.stringify(values.slice(currentIndex, currentIndex + 25).map((line) => line.value)) + ';');
-          res.write('var moreLabels = ' + JSON.stringify(values.slice(currentIndex, currentIndex + 25).map((line) => line.date)) + ';');
-          res.write('myChart.data.labels = myChart.data.labels.concat(moreLabels);');
-          res.write('myChart.data.datasets[0].data = myChart.data.datasets[0].data.concat(moreData);');
-          res.write('myChart.update();');
-          res.write('});');
-
-          res.write('</script>');
-
-          res.write('</body></html>');
-          res.end();
-        }
-
-        // Initially, display the first 25 records
-        sendValues();
+        res.write('</body></html>');
+        res.end();
       });
     });
   }
