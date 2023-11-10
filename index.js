8e4dc6d0c274aace6e80028d3023243888520606
Joeri1983 <96566809+Joeri1983@users.noreply.github.com>
2023-11-10 13:56:00 +0100

    Update index.js

--- index.js
+++ index.js
@@ -7,6 +7,14 @@ const azureStorageUrl = 'https://storagejoeri.blob.core.windows.net/dgjoeri/waar
 
 const server = http.createServer((req, res) => {
   if (req.method === 'GET') {
+    let recordsToShow = 25; // Default number of records to show
+    if (req.url.includes('/show')) {
+      const requestedRecords = req.url.split('/show')[1];
+      if (requestedRecords === '25' || requestedRecords === '50' || requestedRecords === '100' || requestedRecords === '250') {
+        recordsToShow = parseInt(requestedRecords);
+      }
+    }
+
     // Fetch and display the contents of waardes.csv
     https.get(azureStorageUrl, (response) => {
       let data = '';
@@ -24,13 +32,17 @@ const server = http.createServer((req, res) => {
           };
         });
 
-        // Reverse the arrays to display values from newest to oldest
-        const reversedDates = values.map((line) => line.date).reverse();
-        const reversedValues = values.map((line) => line.value).reverse();
+        const latestValues = values.slice(-recordsToShow); // Select the latest records
 
         res.statusCode = 200;
         res.setHeader('Content-Type', 'text/html');
         res.write('<html><body>');
+
+        // Buttons to choose the number of records to display
+        res.write(`<button onclick="window.location.href='/show25'">Show 25 records</button>`);
+        res.write(`<button onclick="window.location.href='/show50'">Show 50 records</button>`);
+        res.write(`<button onclick="window.location.href='/show100'">Show 100 records</button>`);
+        res.write(`<button onclick="window.location.href='/show250'">Show 250 records</button>`);
 
         // Create a canvas for the chart
         res.write('<canvas id="myChart" width="400" height="200"></canvas>');
@@ -39,8 +51,8 @@ const server = http.createServer((req, res) => {
         res.write('<script src="https://cdn.jsdelivr.net/npm/chart.js"></script>');
         res.write('<script>');
         res.write('var ctx = document.getElementById("myChart").getContext("2d");');
-        res.write('var labels = ' + JSON.stringify(reversedDates) + ';');
-        res.write('var data = ' + JSON.stringify(reversedValues) + ';');
+        res.write('var labels = ' + JSON.stringify(latestValues.map((line) => line.date)) + ';');
+        res.write('var data = ' + JSON.stringify(latestValues.map((line) => line.value)) + ';');
         res.write('var myChart = new Chart(ctx, {');
         res.write('type: "line",');
         res.write('data: {');
